7fbe2e787c7be540fed39a3dd1e328b6af0c8025
SK Mehedi Hasan <82324698+mehedishopnil@users.noreply.github.com>
2024-02-04 01:27:33 +0600

    profile part

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -4,11 +4,11 @@ const Profile = () => {
   return (
     <div className="container mx-auto my-10">
       {/* Web Section */}
-      <div className="grid grid-cols-2 p-5 m-5 border rounded-lg">
+      <div className="grid grid-cols-5 p-5 m-10 border rounded-[30px] shadow-sm">
         {/* Left Column */}
-        <div className="lg:w-1/3 pr-8">
+        <div className="flex justify-end items-center col-span-3 pr-8">
           {/* Your left column content goes here */}
-          <div className="mb-4">
+          <div className="flex flex-col items-center mb-4">
             <img
               className="rounded-full w-20 h-20"
               src="https://openclipart.org/download/247319/abstract-user-flat-3.svg" // Replace with your image URL
@@ -16,7 +16,7 @@ const Profile = () => {
             />
             {/* Only show on mobile */}
             <div className="mt-2 lg:hidden text-center"> {/* Centered alignment for mobile */}
-              <h2 className="text-xl font-semibold">Your Name</h2>
+              <h2 className="text-xl font-semibold">Ibrahim Elsamra</h2>
               <p className="text-gray-500">Superhost</p>
             </div>
           </div>
@@ -24,19 +24,19 @@ const Profile = () => {
         </div>
 
         {/* Right Column */}
-        <div className="lg:w-2/3">
+        <div className="flex flex-col items-center text-left col-span-2">
           {/* Your right column content goes here */}
           <div className="mb-4">
-            <h2 className="text-2xl font-semibold">Reviews</h2>
-            <p>Some review details...</p>
+            <h2 className="text-2xl font-bold">88</h2>
+            <p>Reviews</p>
           </div>
           <div className="mb-4">
-            <h2 className="text-2xl font-semibold">Rating</h2>
-            <p>Some rating details...</p>
+            <h2 className="text-2xl font-semibold">4.8</h2>
+            <p>Rating</p>
           </div>
           <div className="mb-4">
-            <h2 className="text-2xl font-semibold">Years Hosting</h2>
-            <p>Some hosting details...</p>
+            <h2 className="text-2xl font-semibold">3</h2>
+            <p>Hosting</p>
           </div>
           {/* Add more content as needed */}
         </div>
@@ -45,7 +45,6 @@ const Profile = () => {
       {/* Mobile Section */}
       <div className="lg:hidden">
         {/* Top Section */}
-        
 
         {/* Bottom Section */}
         <div className="text-center">
